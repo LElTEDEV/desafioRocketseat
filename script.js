35cacee9817c08596de1817d4a50d4451dbd8f1e
LElTEDEV <168042208+LElTEDEV@users.noreply.github.com>
2024-04-25 23:30:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//1
+alert("Hello World");
+//2
+const primeiro = 5;
+const segundo = 5;
+console.log(primeiro + segundo);
+//3
+const compare = 200;
+if (typeof compare === "number") {
+  console.log("É um número");
+} else {
+  console.log("Não é um número.");
+}
+//4
+let eString = "opa";
+if (typeof eString === "string") {
+  console.log("É uma string");
+} else {
+  console.log("Não é uma string");
+}
+//5
+let eBoolean = true;
+if (typeof eBoolean === "boolean") {
+  console.log("É um Boolean");
+} else {
+  console.log("Não é um Boolean");
+}
+//6
+let a = 1;
+let b = 2;
+console.log(a - b);
+//7
+a = 1;
+b = 4;
+console.log(a * b);
+//8
+a = 10;
+b = 5;
+console.log(a / b);
+//9
+let par = 10;
+par % 2 === 0 ? console.log("É par") : console.log("É impar");
+//10
+par % 2 !== 0 ? console.log("É impar") : console.log("É par");
